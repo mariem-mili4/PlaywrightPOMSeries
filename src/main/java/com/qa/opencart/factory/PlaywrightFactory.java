ed69a5a0c04d3477810b55970c63ba18347b10e2
mariem-mili4 <milimaryeem@gmail.com>
2025-12-02 14:23:50 +0100

    ADD/initBrowser : + Check the browser name and launch the corresponding one

--- src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
+++ src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
@@ -1,5 +1,6 @@
 package com.qa.opencart.factory;
 
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Playwright;
 //🏭 Factory = مصنع
 public class PlaywrightFactory {
@@ -8,6 +9,13 @@ public class PlaywrightFactory {
     public void initBrowser (String browserName) {
         System.out.println("Browser name is :"+browserName);
         playwright = Playwright.create(); //create a Playwright instance
+        switch (browserName) {
+            case "chrome" : playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)); break;
+            case "safari" : playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false)); break;
+            case "chromium" : playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)); break;
+            case "firefox" : playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false)); break;
+            default: System.out.println("Browser not found");
+        }
 
     }
 }
